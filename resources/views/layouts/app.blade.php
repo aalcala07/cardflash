fa22b9f999cc7830ff3a5ed41e62d872fbc47d2c
Anthony Alcala <anthony@aalcala.com>
2020-09-01 18:02:55 -0500

    Temporary fix for dropdowns because of missing bootstrap and popper.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
     <title>CardFlash</title>
 
     <!-- Scripts -->
+    <script src="/js/app.js" defer></script>
     <script src="{{ mix('js/app.js', 'vendor/cardflash') }}" defer></script>
 
     <!-- Fonts -->
